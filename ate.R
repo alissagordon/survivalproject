dfac69c680e7ebaf26ebc1589353b00129424e33
alissagordon <99290922+alissagordon@users.noreply.github.com>
2023-11-29 22:46:04 -0800

    Update ate.R

--- ate.R
+++ ate.R
@@ -6,6 +6,9 @@ library(imputeMissings)
 library(tidyverse)
 library(ltmle)
 
+
+
+
 data <- bladder1
 tau=24
 data <- data %>% filter(enum == 1) %>%
@@ -19,7 +22,8 @@ data$status[data$status == 3] <- 0
 data$Y <- ifelse(data$stop <= tau & data$status==1, 1, 0)
 
 #Censoring variable: I(C>tau)
-data$censoring<-ifelse(data$stop>tau & data$status==0, 0,1)
+data$censoring<-ifelse(data$stop>tau & data$status==0, 1,0)
+data$delta=1-data$censoring
 #redefine treatment to numeric
 #0 = placebo, 1 = pyridoxine, 2 = thiotepa
 data$treatment <- as.numeric(data$treatment) - 1
@@ -38,19 +42,54 @@ data01 <- data %>% filter(treatment %in% c(0,1))
 data02 <- data %>% filter(treatment %in% c(0,2))
 data12 <- data %>% filter(treatment %in% c(1,2))
 
-tmp <- data01
+tmp<-data01
+tmle01_48=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
 
-tmle(Y = tmp$Y,
+data02$treatment[data02$treatment==2]<-1
+tmp<-data02
+tmle02_48=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
+
+tmp=data12 %>% mutate(treatment=as.numeric(treatment)-1)
+tmle12_48=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
+
+tmp <- data01
+tmle01_36=tmle(Y = tmp$Y,
      A = tmp$treatment,
      W = tmp %>% dplyr::select(c(size,number)),
-     Delta = tmp$censoring,
+     Delta = tmp$delta,
      family = "binomial")
 
+data02$treatment[data02$treatment==2]<-1
+tmp<-data02
+tmle02_36=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
 
+tmp=data12 %>% mutate(treatment=as.numeric(treatment)-1)
+tmle12_36=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
+tmp=data01
 tmle01_24=tmle(Y = tmp$Y,
      A = tmp$treatment,
      W = tmp %>% dplyr::select(c(size,number)),
-     Delta = tmp$censoring,
+     Delta = tmp$delta,
      family = "binomial")
 
 data02$treatment[data02$treatment==2]<-1
@@ -58,22 +97,43 @@ tmp<-data02
 tmle02_24=tmle(Y = tmp$Y,
                A = tmp$treatment,
                W = tmp %>% dplyr::select(c(size,number)),
-               Delta = tmp$censoring,
+               Delta = tmp$delta,
                family = "binomial")
 
 tmp=data12 %>% mutate(treatment=as.numeric(treatment)-1)
 tmle12_24=tmle(Y = tmp$Y,
                A = tmp$treatment,
                W = tmp %>% dplyr::select(c(size,number)),
-               Delta = tmp$censoring,
+               Delta = tmp$delta,
                family = "binomial")
 
+tmp=data01
+tmle01_12=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
+
+data02$treatment[data02$treatment==2]<-1
+tmp<-data02
+tmle02_12=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
+
+tmp=data12 %>% mutate(treatment=as.numeric(treatment)-1)
+tmle12_12=tmle(Y = tmp$Y,
+               A = tmp$treatment,
+               W = tmp %>% dplyr::select(c(size,number)),
+               Delta = tmp$delta,
+               family = "binomial")
 
 #km curve
 kmdata <- bladder1
 
 
-kmdata <- kmdata %>% filter(enum == 2) %>%
+kmdata <- kmdata %>% filter(enum == 1) %>%
   dplyr::select(-c(rtumor,rsize,enum,recur))
 kmdata<-kmdata %>% mutate(time=stop-start)
 
