24ae81395c0031173e1d8e4c3d4d4626c401b89b
Xyc-arch <66819611+Xyc-arch@users.noreply.github.com>
2023-12-03 22:26:30 -0800

    change cox and upload sim ci ltmle

--- cox_hazards.R
+++ cox_hazards.R
@@ -11,6 +11,8 @@ library(dplyr)
 library(glmnet)
 library(survminer)
 
+
+sim.gate <- TRUE
 data <- bladder1
 
 data <- data %>% 
@@ -30,6 +32,8 @@ data <- data %>% filter(start == 0) %>%
   mutate(time = stop) %>%
   dplyr::select(-c(start,stop,id))
 
+if (sim.gate) data <- read.table("sim_data_1e4.csv", sep= ",", header=TRUE)[,-1]
+
 full_data <- data
 
 #make 3 different datasets for 3 levels of comparison
@@ -38,11 +42,13 @@ data02 <- data %>% filter(treatment %in% c(0,2))
 data12 <- data %>% filter(treatment %in% c(1,2))
 
 data0 <- data %>% filter(treatment == 0)
+data1 <- data %>% filter(treatment == 1)
+data2 <- data %>% filter(treatment == 2)
 
 #data <- data01
 #data <- data02
 #data$treatment[data$treatment == 2] <- 1
-data <- data0
+data <- data1
 data$treatment <- data$treatment - 1
 
 data <- data[data$time != 0, ]
@@ -53,11 +59,7 @@ x <- as.matrix(data[c(2,3)])
 
 fit <- glmnet(x, y, family = "cox")
 
-
-
 plot(survival::survfit(fit, s = 0.05, x = x, y = y), main = "T0 survival")
-
-
 
 
 #######################################
@@ -83,75 +85,37 @@ data <- data %>%
   filter(start == 0) %>%
   mutate(time = stop) %>%
   dplyr::select(-c(start, stop, id))
-
+if (sim.gate) data <- read.table("sim_data_1e4.csv", sep= ",", header=TRUE)[,-1]
 # Fit the survival model (assuming Cox proportional hazards model)
 y <- Surv(data$time, data$status)
-x <- as.matrix(data[c("treatment", "number")]) # Assuming 'number' is a relevant variable
+x <- as.matrix(data[c("size", "number")]) # Assuming 'number' is a relevant variable
 
 fit <- coxph(y ~ x, data = data)
 
-# Plot the survival curve with confidence intervals
-autoplot(survfit(fit), conf.int = TRUE, title = "Survival Curve with Confidence Interval")
+# Plot the survival curve with confidence intervals total
+autoplot(survfit(fit), conf.int = TRUE, title = "Survival Curve with Confidence Interval") + ggtitle("total survival") + xlim(0, 60)
 
-
-
-# work now diff treat
-
-
-# Load necessary libraries
-library(survival)
-library(dplyr)
-library(ggplot2)
-library(ggfortify) # For autoplot function
-
-# Load and prepare the dataset
-data <- bladder1 %>% 
-  dplyr::select(-c(rtumor, rsize, enum, recur)) %>% 
-  mutate(status = as.numeric(status == 1), # Assuming 1 is the event, others are censored
-         treatment = as.numeric(treatment) - 1)
-
-# Use only first recurrence for each patient
-data <- data %>%
-  filter(start == 0) %>%
-  mutate(time = stop) %>%
-  dplyr::select(-c(start, stop, id))
 
 # Split data by treatment
 data_list <- split(data, data$treatment)
 
 # Fit survival models and plot for each treatment group
 plots <- lapply(data_list, function(d) {
-  fit <- survfit(Surv(time, status) ~ 1, data = d)
+  y <- Surv(d$time, d$status)
+  x <- as.matrix(d[c("size", "number")])
+  fit <- survfit(coxph(y ~ x, data = d))
   autoplot(fit, conf.int = TRUE) + 
-    ggtitle(paste("Survival Curve for Treatment", unique(d$treatment)))
+    ggtitle(paste("Survival Curve for Treatment", unique(d$treatment))) + xlim(0, 60)
 })
 
-# Display the plots (this depends on your environment, here's one way to do it)
-library(gridExtra)
-do.call(grid.arrange, c(plots, ncol = 1))
-
-
-for(i in seq_along(data_list)) {
-  fit <- survfit(Surv(time, status) ~ 1, data = data_list[[i]])
-  plot <- autoplot(fit, conf.int = TRUE) + 
-    ggtitle(paste("Survival Curve for Treatment", i - 1)) # Treatment group title
-  
-  # Save or display the plot
-  print(plot) # Use ggsave() if you want to save the plot to a file
+for (p in plots) {
+  print(p)
 }
 
 
 
-# Fit survival model for each treatment
-fit <- survfit(Surv(time, status) ~ treatment, data = data)
 
-print(summary(fit))
 
-# Plot combined survival curves
-ggsurvplot(fit, data = data, 
-           conf.int = TRUE,
-           palette = c("blue", "red", "green"), # Customize colors as needed
-           ggtheme = theme_minimal(),
-           xlab = "Time", 
-           ylab = "Survival Probability",
-           title = "Survival Curves by Treatment Group")
+
+
+
